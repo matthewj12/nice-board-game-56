e8045c1b3a548d68c8a48a9d91a943ee7db45a6c
LindseyArndt <76603241+LindseyArndt@users.noreply.github.com>
2023-02-12 17:22:39 -0600

    added the labels

--- home-page-gui.py
+++ home-page-gui.py
@@ -8,28 +8,28 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 #create CTk window like you do with the Tk window
 app = customtkinter.CTk()  
 app.geometry("1500x700")
-app.title("Superset Me")
+app.title("Superset Me Home")
 customtkinter.set_widget_scaling(1.1)
 
 label = customtkinter.CTkLabel(master=app,
-                               text= "Players Joined",
-                               width=200,
-                               height=70,
-                               fg_color=("white", "gray"),
-                               corner_radius=8,
-                               font=('Times New Roman',25))
-
-label.place(relx=0.38, rely=0.1, anchor=tkinter.W)
-
-label = customtkinter.CTkLabel(master=app,
-                               text= "1: ",
+                               text= "The game will begin when there are 4 connected players \nPlayers Joined:",
                                width=150,
                                height=25,
                                fg_color=("white", "gray"),
                                corner_radius=8,
                                font=('Times New Roman',25))
 
-label.place(relx=0.4, rely=0.35, anchor=tkinter.W)
+label.place(relx=0.3, rely=0.1, anchor=tkinter.W)
+
+label = customtkinter.CTkLabel(master=app,
+                               text= "1: ",
+                               width=100,
+                               height=25,
+                               fg_color=("white", "gray"),
+                               corner_radius=8,
+                               font=('Times New Roman',25))
+
+label.place(relx=0.4, rely=0.2, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text= "2:",
@@ -39,7 +39,7 @@ label = customtkinter.CTkLabel(master=app,
                                corner_radius=8,
                                font=('Times New Roman',25))
 
-label.place(relx=0.37, rely=0.6, anchor=tkinter.W)
+label.place(relx=0.4, rely=0.3, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text= "3:",
@@ -49,7 +49,7 @@ label = customtkinter.CTkLabel(master=app,
                                corner_radius=8,
                                font=('Times New Roman',25))
 
-label.place(relx=0.37, rely=0.6, anchor=tkinter.W)
+label.place(relx=0.4, rely=0.4, anchor=tkinter.W)
 
 label = customtkinter.CTkLabel(master=app,
                                text= "4:",
@@ -59,4 +59,6 @@ label = customtkinter.CTkLabel(master=app,
                                corner_radius=8,
                                font=('Times New Roman',25))
 
-label.place(relx=0.37, rely=0.6, anchor=tkinter.W)
+label.place(relx=0.4, rely=0.5, anchor=tkinter.W)
+
+app.mainloop()
