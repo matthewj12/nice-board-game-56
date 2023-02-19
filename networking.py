144cf13e91aa919c231b11f90b0a0c4c39a0f40b
LindseyArndt <76603241+LindseyArndt@users.noreply.github.com>
2023-02-19 15:32:57 -0600

    adding comments

--- networking.py
+++ networking.py
@@ -1,6 +1,6 @@
 import socket, pickle
 
-
+#we use this to connect each time we want to send or receive something
 class Connection():
 	# string representing an IPv4 address (e.g., '10.34.1.203')
 	ip_addr = None
@@ -17,7 +17,7 @@ class Connection():
 def getIPaddr():
 	return socket.gethostbyname(socket.gethostname())
 
-
+#use to send game state to server
 def sendGameState(dest_ip, dest_port, pack_size, gs):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((dest_ip, dest_port))
@@ -27,6 +27,7 @@ def sendGameState(dest_ip, dest_port, pack_size, gs):
 
 	sock.close()
 
+#use to get the game state
 def recvGameState(source_ip, source_port, pack_size):
 	sock = socket.socket(socket.AF_INSET, socket.SOCK_STREAM)
 	sock.bind((source_ip, source_port))
